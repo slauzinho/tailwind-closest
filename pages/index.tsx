54354d046a66d7f0c7279183282d74b58909dbdb
teerryn <slaughterer90@gmail.com>
2021-06-18 20:44:49 +0100

    Small QoL improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,35 +3,68 @@ import { useEffect, useRef, useState } from 'react';
 import useColour from '../hooks/useColour';
 
 export default function Home() {
-  const [userInput, setUserInput] = useState<string>();
+  const [userInput, setUserInput] = useState<string>('');
   const tailwind = useColour(userInput);
 
-  const colorInput = useRef<HTMLInputElement>()
+  const colorInput = useRef<HTMLInputElement>();
+
+  useEffect(() => {
+    if (userInput && !userInput.startsWith('#')) {
+      setUserInput((usrInput) => `#${usrInput}`);
+    }
+  }, [userInput]);
 
   useEffect(() => {
     colorInput.current?.focus();
-  }, [])
+  }, []);
 
   return (
     <div>
       <Head>
         <title>Tailwind Closes Color</title>
-        <meta name="description" content="App that provided with a color will find the closest equivalent in tailwind" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <meta
+          name="description"
+          content="App that provided with a color will find the closest equivalent in tailwind"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main className="relative flex flex-col items-center min-h-screen">
         <div className="z-10 flex items-baseline justify-center w-full h-screen mt-10 lg:mt-0 lg:items-center">
           <input
-          ref={colorInput}
+            ref={colorInput}
             autoFocus
             type="text"
             placeholder="Enter your hexadecimal colour..."
             className="px-10 py-6 text-base leading-6 text-gray-500 placeholder-gray-500 border-2 border-gray-300 shadow-md lg:w-1/4 rounded-2xl focus:outline-none focus:placeholder-gray-400"
+            value={userInput}
             onChange={(e) => setUserInput(e.target.value)}
+            onBlur={() => {
+              if (userInput.length > 7) {
+                const re = /^#(?:[0-9a-fA-F]{3}){1,2}/;
+                const result = re.exec(userInput);
+                if (result) {
+                  return setUserInput(result[0]);
+                }
+              }
+            }}
           />
         </div>
         <div className="absolute flex flex-1 w-full h-screen">
